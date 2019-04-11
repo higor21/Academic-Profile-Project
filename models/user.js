7f9a25afcff358b78092af11fa2f217658afc3a8
higor21 <felype.cet.15@outlook.com>
2019-04-10 23:13:02 -0300

    adding new project

---
+++ models/user.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose")
+var passportLocalMongoose = require("passport-local-mongoose")
+
+var UserSchema = new mongoose.Schema({
+    image_url: String, // It has to change (image from upload)
+    username: String,
+    firstname: String,
+    lastname: String,
+    description: String,
+    occupation: String,
+    phone_number: String,
+    home_page_link: String,
+    linkedin_link: String,
+    lattes_link: String,
+    github_link: String,
+    password: String,
+    email: String,
+    isAdmin: {type: Boolean, default: false},
+    skills: []
+})
+
+UserSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model("User", UserSchema)

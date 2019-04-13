a613d3b87e2d62fc7c828a3d55507ad9b2c74ba3
higor21 <felype.cet.15@outlook.com>
2019-04-13 16:48:59 -0300

    update from features list files to put in project

--- Todo-List-Project/assets/js/todos.js
+++ Todo-List-Project/assets/js/todos.js
@@ -1,26 +1,76 @@
-// Check Off Specific Todos By Clicking
-/*$("ul").on("click", "li", function(){
-	$(this).toggleClass("completed");
-});*/
 
-//Click on X to delete Todo
-$("ul").on("click", "span", function(event){
-	$(this).parent().fadeOut(500,function(){
-		$(this).remove();
-	});
-	event.stopPropagation();
-});
+var itens = []
 
-$("input[type='text']").keypress(function(event){
+// ready
+var reload_itens = function(){
+	let i = 0, j;
+	$('ul').empty()
+	itens.forEach(elem => {
+		let str = 	"<li class='item "+ i +"'>"+
+						"<span class='hf_trash'><i class='fa fa-trash'></i></span>" +
+						elem.title +
+						"<span class='hf_plus'><i class='fa fa-plus'></i></span>" +
+					"</li>" +
+					"<input id='add_subitem_"+ i +"' type='text' placeholder='Add SubItem'>"
+		j = 0;
+		elem.sub_itens.forEach(s_e => {
+			str += 	"<li class='sub_item "+ i +"_"+ (j++) +"'><span class='hf_trash'><i class='fa fa-trash'></i></span>" +
+						"<span class='hf_angle'><i class='fas fa-angle-right'></i></span>" +
+						s_e.topic +
+					"</li>"
+		})
+		i++;
+		$("ul").append(str)
+	})
+}
+
+// ready
+var add_itens = function(obj, event){
 	if(event.which === 13){
 		//grabbing new todo text from input
-		var todoText = $(this).val();
-		$(this).val("");
+		var todoText = $(obj).val();
+		$(obj).val("");
 		//create a new li and add to ul
-		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+		if(obj.id == "add_item")
+			itens.push({ title: todoText, sub_itens: []})
+		else{
+			var index_si = parseInt(obj.id.split('_')[2])
+			itens[index_si].sub_itens.push({topic: todoText})
+		}
+		reload_itens()
 	}
+}
+
+// ready
+$("#add_item").keypress(function(event){
+	add_itens(this, event)
 });
 
-$(".fa-plus").click(function(){
-	$("input[type='text']").fadeToggle();
+
+// ready
+$("ul").on("keydown","[id*='add_subitem']", function(event){
+	add_itens(this, event)
+});
+
+// ready
+$("ul").on("click", ".fa-plus", function(){
+	var input_i = $(this).parent().parent()[0].className.split(' ')[1]
+	$('#add_subitem_' + input_i).fadeToggle();
+});
+
+$("h1 > .fa-plus").click(function(){
+	$("#add_item").fadeToggle();
+})
+
+// ready
+$("ul").on("click", ".hf_trash", function(event){
+	if($(this).parent()[0].className.split(' ')[0] == 'item'){
+		pos = itens.map(e => {return e.title}).indexOf($(this).parent()[0].textContent)
+		if(pos > -1) itens.splice(pos, 1)
+	}else{
+		item_i = parseInt($(this).parent()[0].className.split(' ')[1][0])
+		subitem_i = parseInt($(this).parent()[0].className.split(' ')[1][2])
+		itens[item_i].sub_itens.splice(subitem_i, 1);
+	}
+	reload_itens()
 });
